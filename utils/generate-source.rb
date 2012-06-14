605c7dff40a6825e50d6df56cfa0c64fd2a687ea
Chase Miller <chmille4@gmail.com>
2012-06-14 14:22:35 -0400

    Updated annotated source code for version 1.1.3

--- utils/generate-source.rb
+++ utils/generate-source.rb
@@ -1,6 +1,6 @@
 # generate html
   Dir.chdir('../src')
-  `dox --ribbon 'https://github.com/chmille4/Scribl' --title Scribl --desc "HTML5 Canvas Genomics Graphics Library" Scribl.js Scribl.track.js Scribl.lane.js Scribl.glyph.js Scribl.events.js Scribl.tooltips.js Scribl.class.js Scribl.utils.js Scribl.svg.js glyph/Scribl.arrow.js glyph/Scribl.blockarrow.js glyph/Scribl.complex.js glyph/Scribl.line.js glyph/Scribl.rect.js parsers/bed.js parsers/genbank.js > ../utils/presource.html`
+  `dox --ribbon 'https://github.com/chmille4/Scribl' --title Scribl --desc "HTML5 Canvas Genomics Graphics Library" Scribl.js Scribl.track.js Scribl.lane.js Scribl.glyph.js Scribl.events.js Scribl.tooltips.js Scribl.utils.js Scribl.svg.js glyph/Scribl.arrow.js glyph/Scribl.blockarrow.js glyph/Scribl.seq.js glyph/Scribl.complex.js glyph/Scribl.line.js glyph/Scribl.rect.js parsers/bed.js parsers/genbank.js > ../utils/presource.html`
   Dir.chdir('../utils')
 
 # fix issue with special symbols
